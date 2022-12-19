ef208abe0445506bec778792d2854fee6518d99b
Fateema Shu'ayb <fatimashuaib6@gmail.com>
2022-12-19 15:09:14 -0800

    task

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-=======
-afe_print_list(my_list=[], x=0):
->>>>>>> 1f4b4c3ca5d8ea8ce31bf594d6aca9c57f71235f
     count = 0
     for i in range(0, x):
         try:
